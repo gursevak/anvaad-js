506cf5707455750b49638450d5b5b04d2e0b9825
Irvanjit Singh <irvanjit@gmail.com>
2020-04-12 21:29:41 -0400

    added csv reader to callable functions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const firstLetters = require('./firstLetters');
 const mainLetters = require('./mainLetters');
 const translit = require('./translit');
 const unicode = require('./unicode');
+const readCsvWordMap = require('./readCsvWordMap');
 
 module.exports = {
   alphabetize,
@@ -12,4 +13,5 @@ module.exports = {
   mainLetters,
   translit,
   unicode,
+  readCsvWordMap,
 };
